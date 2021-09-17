aab388f710e1816415ec9c9266b2b9936fb55307
Jero Gaming <mw.sevice@gmail.com>
2021-09-17 17:36:36 +0700

    Ver 1

---
+++ src/YTBJero/pickaxe/Score.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace YTBJero\pickaxe;
+
+use pocketmine\scheduler\Task;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use LousWiteMC\PhoBan\Main;
+use ScoreMC\ScoreMC;
+use pocketmine\level\Level;
+use pocketmine\Player;
+class Score extends Task{
+
+	public function onRun(int $tick) : void{
+		foreach(Server::getInstance()->getOnlinePlayers() as $player){
+			if($player->getLevel()->getName() == "sb"){
+				$lv = Pickaxe::getInstance()->getLevel($player);
+				$xp = Pickaxe::getInstance()->getExp($player);
+				$nxp = Pickaxe::getInstance()->getNextExp($player);
+				ScoreMC::getInstance()->createScore($player, '§l§ePICKAXE LEVEL');
+				ScoreMC::getInstance()->setScoreLines($player, ["", "§c§l⊱ §bCấp Cúp: §a {$lv}", "§c§l⊱ §bKinh Nghiệm:§a {$xp}§3/§a{$nxp}", "", "§fIP: §ewww.example.com"]);
+			}
+		}
+	}
+}
